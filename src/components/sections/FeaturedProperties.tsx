37fcdbf23e788042ee2409a3cae09f72e7cf72be
MimoSYR <mimo.syr.syr@gmail.com>
2026-01-15 20:59:24 +0300

    refactor: clean up unnecessary code lines

--- src/components/sections/FeaturedProperties.tsx
+++ src/components/sections/FeaturedProperties.tsx
@@ -1,16 +1,5 @@
-import SectionHeader from "../ui/SectionHeader";
-
-// const fpInfo = {
-//   title: "Featured Properties",
-//   text: 'Explore our handpicked selection of featured properties. Each listing offers a glimpse into exceptional homes and investments available through Estatein.',
-//   extraText: 'Click "View Details" for more information.',
-//   btnText: "View All Properties",
-// };
-
 const FeaturedProperties = () => {
-  return <div>FeaturedProperties
-    {/* <SectionHeader {...fpInfo} /> */}
-  </div>;
+  return <div>FeaturedProperties</div>;
 };
 
 export default FeaturedProperties;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import SectionContainer from "../components/layouts/SectionContainer";
-import FeaturedProperties from "../components/sections/FeaturedProperties";
 import InfoCardsSection from "../components/sections/InfoCardsSection";
 import { infoCardsData } from "../data/homeData";
 
 const Home = () => {
   return (
     <div className="h-screen">
-      Home
       <section className="mx-4 sm:mx-20 lg:mx-0">
         <InfoCardsSection cards={infoCardsData} />
       </section>
