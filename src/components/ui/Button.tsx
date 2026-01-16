a5d866666586f9296728b028fbd63b14180e19e6
RaghadDeveloper <raghadmh620@gmail.com>
2026-01-16 11:17:56 +0300

    add HomeHero section

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,18 @@
-const Button = () => {
-  return <div>Button</div>;
+import type { ButtonProps } from "../../interfaces";
+
+const Button = ({ variant, onClick, children }: ButtonProps) => {
+  const styles = {
+    base: "px-5 py-3.5 2xl:px-6 2xl:py-4.5 rounded-lg 2xl:rounded-[10px] text-[14px] 2xl:text-lg cursor-pointer",
+    primary: "bg-primary-60",
+    secondary: "bg-gray-10 border border-gray-15",
+    border: "border border-gray-15",
+  };
+
+  return (
+    <button className={styles.base + " " + styles[variant]} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,3 +3,14 @@ export interface SliderProps {
   cardsPerView?: number;
   spaceBetween?: number;
 }
+
+export interface ButtonProps {
+  variant: "primary" | "secondary" | "border";
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+export interface StatisticCardType {
+  value: string;
+  text: string;
+}
