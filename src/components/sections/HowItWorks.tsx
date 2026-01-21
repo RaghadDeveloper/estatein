ae8a7169c42b6ca7f5a7564bfdacfeede1a62b16
Raghadalalai1 <raghadalali63@gmail.com>
2026-01-21 20:33:30 +0300

    edit HowItWork section

--- src/components/sections/HowItWorks.tsx
+++ src/components/sections/HowItWorks.tsx
@@ -10,13 +10,17 @@ const HowItWorks = () => {
         <SectionHeader {...howItWorksHeaderData} />
         <div className="max-w-399 mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10 2xl:gap-x-8">
-            {aboutData.map((step) => (
-              <StepCard
+            {aboutData.map((step, index) => (
+              <div
                 key={step.id}
-                stepNumber={step.stepNumber}
-                title={step.title}
-                description={step.description}
-              />
+                className={index >= 3 ? "hidden md:block" : "block"}
+              >
+                <StepCard
+                  stepNumber={step.stepNumber}
+                  title={step.title}
+                  description={step.description}
+                />
+              </div>
             ))}
           </div>
         </div>
@@ -24,5 +28,4 @@ const HowItWorks = () => {
     </section>
   );
 };
-
 export default HowItWorks;
