0f19553c2ae368468ce20840b19c8e551787529f
RawanBarakat-hub <rawan.20.2.1@icloud.com>
2026-01-28 01:57:17 -0900

    loader

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,17 +1,36 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import AnnouncementBar from "../ui/AnnouncementBar";
 import Navbar from "../ui/Navbar";
 import Footer from "../ui/Footer";
+import Loader from "../ui/Loader";
 
 const MainLayout = () => {
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 1500); // ⏱️ ثانيتين
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="font-urbanist bg-gray-08 text-white">
+    <div className="min-h-screen flex flex-col bg-gray-08 text-white">
       <AnnouncementBar />
       <Navbar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
+
+      <div className="flex-1 flex flex-col justify-center items-center">
+        {showLoader ? (
+          <Loader />
+        ) : (
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        )}
+      </div>
+
       <Footer />
     </div>
   );
---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,10 @@
+import {Atom} from "react-loading-indicators"
+const Loader = () => {
+    return (
+        <div>
+            <Atom color="#703bf7"  />
+        </div>
+    );
+};
+
+export default Loader;
